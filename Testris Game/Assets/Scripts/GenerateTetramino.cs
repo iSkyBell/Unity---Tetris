19c41519b9b7e0dfc6d7975b3e6e5aed364fac4d
iSkyBell <hernan.pk92@hotmail.com>
2020-06-21 18:50:39 -0500

    Se agrega la funcionalidad de: - La creación del objecto, que se genere cada vez que se complete. - La creación de la pieza y la funcionalidad de: Ir hacia los lados y bajar rapido.

---
+++ Testris Game/Assets/Scripts/GenerateTetramino.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTetramino : MonoBehaviour
+{
+
+    #region variables
+    public GameObject[] tetramino;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newTetramino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+     * Metodo que generara nuevas piezas del tetris.
+     * */
+    public void newTetramino()
+    {
+        Instantiate(tetramino[Random.Range(0,tetramino.Length)], transform.position, Quaternion.identity);
+    }
+}
---
+++ Testris Game/Assets/Scripts/LogicTetramino.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogicTetramino : MonoBehaviour
+{
+
+    #region variables
+    private float previousTime;
+    public float timeDown = 0.8f;
+    public static int high = 20;
+    public static int width = 10;
+    public Vector3 rotationPoint;
+    private static Transform[,] grid = new Transform[width, high];
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!limits())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!limits())
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? timeDown / 20 : timeDown))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!limits())
+            {
+                transform.position += new Vector3(0, 1, 0);
+                addGrid();
+                this.enabled = false;
+                FindObjectOfType<GenerateTetramino>().newTetramino();
+            }
+
+            previousTime = Time.time;
+        }
+    }
+
+    /**
+     * Metodo que sirve para que el objecto no pase del limite del fondo.
+     * Si el objecto llega al limite del fondo, o sea, por abajo no lo deja avanzar mas y detiene el objecto.
+     * @return Retorna un booleano en caso de que supere el limite es false y no lo deja.
+     * */
+    bool limits()
+    {
+        foreach (Transform transformChild in transform)
+        {
+            int wholeX = Mathf.RoundToInt(transformChild.transform.position.x);
+            int wholeY = Mathf.RoundToInt(transformChild.transform.position.y);
+            if(wholeX < 0 || wholeX >= width || wholeY < 0 || wholeY >= high)
+            {
+                return false;
+            }
+
+            if(grid[wholeX,wholeY] != null)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    /**
+     * Metodo que nos ayuda con el transform para agregar que en la posición tal ya hay un objecto.
+     * */
+    void addGrid()
+    {
+        foreach (Transform transformChild in transform)
+        {
+            int wholeX = Mathf.RoundToInt(transformChild.transform.position.x);
+            int wholeY = Mathf.RoundToInt(transformChild.transform.position.y);
+
+            grid[wholeX, wholeY] = transformChild;
+
+
+        }
+    }
+}
